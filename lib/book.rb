f9960d73b1482762c4b41bb877d8141bd97c0f68
Zayne Abraham <zayneabraham1@gmail.com>
2017-02-08 21:27:10 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,15 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
   def turn_page
